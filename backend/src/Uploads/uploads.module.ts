25e37cd1ad3a7f6e356b0d55fb84dca8699ca6bf
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-08-09 22:20:51 +0530

    Update uploads.module.ts

--- backend/src/Uploads/uploads.module.ts
+++ backend/src/Uploads/uploads.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UploadsService } from './uploads.service';
-import { UploadsController } from './uploads.controller';
+import { UploadsService } from './Uploads.service';
+import { UploadsController } from './Uploads.controller';
 
 @Module({
   controllers: [UploadsController],
